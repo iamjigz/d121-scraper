1f671f5df01d35c2c8b77074580963b7737ccc75
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2018-02-21 07:02:37 +0800

    WIP: Get details object

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
 let app = angular.module('ngApp', ['ngMaterial', 'ngMessages', 'ngAnimate'])
 
-app.controller('AppCtrl', ($scope) => {
+app.controller('AppCtrl', ($scope, $mdToast) => {
+	$scope.toast = (text, type) => {
+		const toast = $mdToast.simple()
+			.textContent(text)
+			.action('OK')
+			.highlightAction(true)
+			.highlightClass(`md-${type}`)
+			.position('top right')
+			.hideDelay(3000)
 
-})
-
-app.directive('pasteDirective', function() {
-	var linkFn = function(scope, element, attrs) {
-
-		element.on('paste', function() {
-
-			setTimeout(function() {
-				console.log(element.val())
-				scope.pastedText = element.val()
-				scope.$apply()
-			}, 5)
-
-		})
-	}
-
-	return {
-		restrict: 'A',
-		link: linkFn
+		$mdToast.show(toast)
 	}
 })
--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,9 +1,12 @@
-app.controller('MainCtrl', ($scope, $http, $timeout, $sce) => {
+app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 
 	$scope.getPdf = (url) => {
-		$scope.status = "Loading..."
+		$scope.status = 'Loading. Please Wait...'
 		$http.get(url, {
-				responseType: 'arraybuffer'
+				responseType: 'arraybuffer',
+				headers: {
+					'Accept': 'application/pdf'
+				}
 			})
 			.success(function(response) {
 				const file = new Blob([(response)], {
@@ -12,13 +15,63 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce) => {
 
 				const fileURL = URL.createObjectURL(file);
 				$scope.pdfContent = $sce.trustAsResourceUrl(fileURL)
-				$scope.status = "Done!"
-			});
+				$scope.toast('All Done', 'primary')
+				$scope.status = ''
+			})
+			.catch(err => {
+				$scope.toast('Please enable Cross-Origin Request.', 'warn')
+				$scope.status = ''
+			})
 	}
 
-	$scope.paste = () => {
+	$scope.handlePaste = (e) => {
+		$timeout(function() {
+			if (e.target.value.length > 0) {
+				const text = e.target.value
+				$scope.copiedText = text
+				getDetails(text)
+			}
+		}, 10)
 
-		let text = $scope.target
-		console.log(text)
+		const getDetails = text => {
+			let details = {}
+
+			arr = text.split('\n')
+			console.log(arr)
+
+			let name = arr[0]
+			let phone = getValue(arr, 'Tel: (.*)')
+			let sic = getValue(arr, 'US SIC: (.*)')
+			let code = getValue(arr, 'US SIC: (\d*).*')
+			let directors = getValue(arr, 'Director.*: (.*)')
+			let emp_size = getValue(arr, 'Emp: (.*)')
+
+			details = {
+				name: name,
+				phone: phone,
+				sic: sic,
+				code: code,
+				directors: directors,
+				emp_size: emp_size
+			}
+
+			console.log(details)
+		}
+
+		const getValue = (arr, text) => {
+			const regex = new RegExp(text, 'g')
+			const string = arr.find(i => regex.test(i))
+
+			if (string) {
+				const match = string.match(regex)[0]
+				console.log(match)
+			}
+		}
+	}
+
+
+
+	$scope.openSidenav = () => {
+		$mdSidenav('right').toggle()
 	}
 })
