8734405e34db145af54d81db40766c568e70cd2e
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2018-02-22 06:13:06 +0800

    Added DataService

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,4 +1,5 @@
-app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
+app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav, $mdDialog, DataService) => {
+	const DS = DataService
 
 	$scope.getPdf = (url) => {
 		$scope.status = 'Loading. Please Wait...'
@@ -15,7 +16,7 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 
 				const fileURL = URL.createObjectURL(file);
 				$scope.pdfContent = $sce.trustAsResourceUrl(fileURL)
-				$scope.toast('All Done', 'primary')
+				$scope.toast('All Done', 'secondary')
 				$scope.status = ''
 			})
 			.catch(err => {
@@ -29,7 +30,7 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 			if (e.target.value.length > 0) {
 				const text = e.target.value
 				$scope.copiedText = text
-				getDetails(text)
+				$scope.details = getDetails(text)
 			}
 		}, 10)
 
@@ -37,15 +38,14 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 			let details = {}
 
 			arr = text.split('\n')
-			console.log(arr)
 
-			let name = arr[0]
-			let address = arr[1]
-			let phone = findByRegex(arr, '(?:Tel:)(.*)')
-			let sic = findByRegex(arr, '(?:US SIC: \\d+\\s)(.*)')
-			let code = findByRegex(arr, '(?:US SIC: )(\\d+)')
-			let directors = findByRegex(arr, '((.*Director|Partner|Proprietor).*: .*)')
-			let emp_size = findByRegex(arr, 'Emp: (.*)')
+			const name = arr[0]
+			const address = arr[1]
+			const phone = findByRegex(arr, '(?:Tel:)(.*)')
+			const sic = findByRegex(arr, '(?:US SIC: \\d+\\s)(.*)')
+			const code = findByRegex(arr, '(?:US SIC: )(\\d+)')
+			const conctact = findByRegex(arr, '((.*Director|Partner|Proprietor).*: .*)')
+			const emp_size = findByRegex(arr, 'Emp: (.*)')
 
 			details = {
 				name: name,
@@ -53,11 +53,12 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 				phone: phone,
 				sic: sic,
 				code: code,
-				directors: directors,
-				emp_size: emp_size
+				conctact: conctact,
+				emp_size: emp_size,
+				note: ''
 			}
 
-			console.log(details)
+			return details
 		}
 
 		const findByRegex = (arr, rgx) => {
@@ -68,9 +69,8 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 				const match = string.match(regex)
 				const index = arr.findIndex(i => regex.test(i))
 				const next = checkNextIndex(arr[index + 1]) ? '' : arr[index + 1]
-				const value = next ? match[1].concat(' ', next) : match[1]
 
-				return value || ''
+				return match[1].concat(' ', next ? next : '').trim() || ''
 			}
 
 			return ''
@@ -82,9 +82,33 @@ app.controller('MainCtrl', ($scope, $http, $timeout, $sce, $mdSidenav) => {
 		}
 	}
 
-
-
 	$scope.openSidenav = () => {
 		$mdSidenav('right').toggle()
+	}
+
+	$scope.edit = (key, value, ev) => {
+		const prompt = $mdDialog.prompt()
+			.title('Edit Detail')
+			.textContent(`Do you want to edit the ${key} of this business?`)
+			.placeholder(`Enter a new ${key}`)
+			.ariaLabel('New Value')
+			.initialValue(value)
+			.targetEvent(ev)
+			.ok('Save')
+			.cancel('Cancel')
+
+		$mdDialog.show(prompt)
+			.then(res => {
+				$scope.details[key] = res
+				$scope.toast(`The value for ${key} has been edited.`, 'secondary')
+			}, () => {
+				$scope.toast(`Editing has been cancelled`, 'warn')
+			})
+	}
+
+	$scope.save = details => {
+		$scope.toast(`Added ${details.name}.`, 'secondary')
+		DS.set(details)
+		$scope.details = ''
 	}
 })
--- js/controllers/output.js
+++ js/controllers/output.js
@@ -1,3 +1,14 @@
-app.controller('OutputCtrl', ($scope) => {
+app.controller('OutputCtrl', ($scope, DataService) => {
+	const DS = DataService
 
+	$scope.$watch(() => {
+		return DS.get()
+	}, (newVal, oldVal) => {
+		$scope.response = newVal
+	})
+
+	$scope.delete = item => {
+		console.log(item)
+		$scope.response.splice($scope.response.indexOf(item), 1)
+	}
 })
